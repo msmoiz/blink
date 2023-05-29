10f3715e837d9d7f4f4f165f9b06c4ca5f3a5c08
msmoiz <mustafa.moiz125@gmail.com>
2023-05-29 13:08:42 -0700

    feat: show blink reminder every 15 mins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use notify_rust::Notification;
+use std::{error::Error, thread, time::Duration};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let message = [
+        "Take a break from the monitor.",
+        "Stare off into the distance.",
+        "Grab a snack.",
+        "Do whatever.",
+        "Just make sure it does not have a screen.",
+    ]
+    .join(" ");
+
+    loop {
+        thread::sleep(Duration::from_secs(15 * 60));
+        Notification::new()
+            .summary("Reminder to blink")
+            .body(&message)
+            .timeout(0) // do not expire, the user should be forced to dismiss
+            .show()?;
+    }
 }
